a647188f574f6f5fd8d990977d3639b9fd1876cf
Bukola <sobowalebukola@gmail.com>
2019-10-17 12:34:42 +0100

    UI fix

--- src/App.js
+++ src/App.js
@@ -1,115 +1,139 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import Tesseract from "tesseract.js";
 
+var styles = {
+  background: "#00F260", /* fallback for old browsers */
+background: "-webkit-linear-gradient(to right, #0575E6, #00F260)",  /* Chrome 10-25, Safari 5.1-6 */
+background: "linear-gradient(to right, #0575E6, #00F260)" ,/* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+height: "60px",
+marginTop: "40px",
+borderRadius: "17px",
+fontSize: "25px"
+
+}
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       uploads: [],
       patterns: [],
       documents: []
     };
   }
-  handleChange = (event) => {
+  handleChange = event => {
     if (event.target.files[0]) {
-      var uploads = []
+      var uploads = [];
       for (var key in event.target.files) {
         if (!event.target.files.hasOwnProperty(key)) continue;
-        let upload = event.target.files[key]
-        uploads.push(URL.createObjectURL(upload))
+        let upload = event.target.files[key];
+        uploads.push(URL.createObjectURL(upload));
       }
       this.setState({
         uploads: uploads
-      })
+      });
     } else {
       this.setState({
         uploads: []
-      })
+      });
     }
-  }
+  };
   generateText = () => {
-    let uploads = this.state.uploads
-  
-    for(var i = 0; i < uploads.length; i++) {
+    let uploads = this.state.uploads;
+
+    for (var i = 0; i < uploads.length; i++) {
       Tesseract.recognize(uploads[i], {
-        lang: 'eng'
+        lang: "eng"
       })
-      .catch(err => {
-        console.error(err)
-      })
-      .then(result => {
-        // Get Confidence score
-        let confidence = result.confidence
-  
-        // Get full output
-        let text = result.text
-  
-        // Get codes
-        let pattern = /\b\w{10,10}\b/g
-        let patterns = result.text.match(pattern);
-  
-        // Update state
-        this.setState({ 
-          patterns: this.state.patterns.concat(patterns),
-          documents: this.state.documents.concat({
-            pattern: patterns,
-            text: text,
-            confidence: confidence
-          })
+        .catch(err => {
+          console.error(err);
         })
-      })
+        .then(result => {
+          // Get Confidence score
+          let confidence = result.confidence;
+
+          // Get full output
+          let text = result.text;
+
+          // Get codes
+          let pattern = /\b\w{10,10}\b/g;
+          let patterns = result.text.match(pattern);
+
+          // Update state
+          this.setState({
+            patterns: this.state.patterns.concat(patterns),
+            documents: this.state.documents.concat({
+              pattern: patterns,
+              text: text,
+              confidence: confidence
+            })
+          });
+        });
     }
-  }
+  };
   render() {
     return (
       <div className="app">
         <header className="header">
-          <h1>My OCR App</h1>
+          <h1>Converts Image to Text</h1>
         </header>
 
         <section className="hero">
-  <label className="fileUploaderContainer">
-    Click here to upload documents
-    <input type="file" id="fileUploader" onChange={this.handleChange} multiple />
-  </label>
+          <label className="fileUploaderContainer">
+            Click here to upload documents
+            <input
+              type="file"
+              id="fileUploader"
+              onChange={this.handleChange}
+              multiple
+            />
+          </label>
 
-  <div>
-    { this.state.uploads.map((value, index) => {
-      return <img key={index} src={value} width="100px" alt = "needed"/>
-    }) }
-  </div>
-
-  <button onClick={this.generateText} className="button">Generate</button>
-</section>
-
-        { /* Results */ }
-        <section className="results">
-  { this.state.documents.map((value, index) => {
-    return (
-      <div key={index} className="results__result">
-        <div className="results__result__image">
-          <img src={this.state.uploads[index]} width="250px"  alt = "upload"/>
-        </div>
-        <div className="results__result__info">
-          <div className="results__result__info__codes">
-            <small><strong>Confidence Score:</strong> {value.confidence}</small>
+          <div>
+            {this.state.uploads.map((value, index) => {
+              return <img key={index} src={value} width="100px" alt="needed"/>;
+            })}
           </div>
-          {/*<div className="results__result__info__codes">
+
+          <button onClick={this.generateText} className="button" style ={styles} >
+            Generate
+          </button>
+        </section>
+
+        {/* Results */}
+        <section className="results">
+          {this.state.documents.map((value, index) => {
+            return (
+              <div key={index} className="results__result">
+                <div className="results__result__image">
+                  <img
+                    src={this.state.uploads[index]}
+                    width="250px"
+                    alt="upload"
+                  />
+                </div>
+                <div className="results__result__info">
+                  <div className="results__result__info__codes">
+                    <small>
+                      <strong>Confidence Score:</strong> {value.confidence}
+                    </small>
+                  </div>
+                  {/*<div className="results__result__info__codes">
             <small><strong>Pattern Output:</strong> {value.pattern.map((pattern) => { return pattern + ', ' })}</small>
     </div> */}
-          <div className="results__result__info__text">
-            <small><strong>Full Output:</strong> {value.text}</small>
-          </div>
-        </div>
+                  <div className="results__result__info__text">
+                    <small>
+                      <strong>Full Output:</strong> {value.text}
+                    </small>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </section>
       </div>
-    )
-  }) }
-</section>
-      </div>
-    )
+    );
   }
-
 }
 
 export default App;
